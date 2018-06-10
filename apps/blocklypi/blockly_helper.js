a5ada772d9c0568094f75ef14b788e9ba860f586
王逸伦 <594352301@qq.com>
2018-06-10 12:17:45 +0800

    no commit message

---
+++ apps/blocklypi/blockly_helper.js
@@ -0,0 +1,163 @@
+/**
+ * Execute the user's code.
+ * Just a quick and dirty eval.  No checks for infinite loops, etc.
+ */
+function runJS() {
+  var code = Blockly.Generator.workspaceToCode('JavaScript');
+  try {
+    eval(code);
+  } catch (e) {
+    alert('Program error:\n' + e);
+  }
+}
+
+/**
+ * Backup code blocks to localStorage.
+ */
+function backup_blocks() {
+  if ('localStorage' in window) {
+    var xml = Blockly.Xml.workspaceToDom(Blockly.mainWorkspace);
+    window.localStorage.setItem('arduino', Blockly.Xml.domToText(xml));
+  }
+}
+
+/**
+ * Restore code blocks from localStorage.
+ */
+function restore_blocks() {
+  if ('localStorage' in window && window.localStorage.arduino) {
+    var xml = Blockly.Xml.textToDom(window.localStorage.arduino);
+    Blockly.Xml.domToWorkspace(Blockly.mainWorkspace, xml);
+  }
+}
+///////////////////
+/**
+* Save Arduino generated code to local file.
+*/
+function saveCode() {
+  var fileName = window.prompt('请输入文件名?', 'BlocklyPi')
+  //doesn't save if the user quits the save prompt
+  if(fileName){
+    var blob = new Blob([Blockly.c.workspaceToCode()], {type: 'text/plain;charset=utf-8'});
+    saveAs(blob, fileName + '.c');
+  }
+}
+
+/**
+ * Save blocks to local file.
+ * better include Blob and FileSaver for browser compatibility
+ */
+function save() {
+  var xml = Blockly.Xml.workspaceToDom(Blockly.mainWorkspace);
+  var data = Blockly.Xml.domToText(xml);
+  var fileName = window.prompt('请输入文件名?', 'BlocklyPi');
+  // Store data in blob.
+  // var builder = new BlobBuilder();
+  // builder.append(data);
+  // saveAs(builder.getBlob('text/plain;charset=utf-8'), 'blockduino.xml');
+  if(fileName){
+    var blob = new Blob([data], {type: 'text/xml'});
+    saveAs(blob, fileName + ".xml");
+  } 
+}
+
+/**
+ * Load blocks from local file.
+ */
+function load(event) {
+  var files = event.target.files;
+  // Only allow uploading one file.
+  if (files.length != 1) {
+    return;
+  }
+
+  // FileReader
+  var reader = new FileReader();
+  reader.onloadend = function(event) {
+    var target = event.target;
+    // 2 == FileReader.DONE
+    if (target.readyState == 2) {
+      try {
+        var xml = Blockly.Xml.textToDom(target.result);
+      } catch (e) {
+        alert('Error parsing XML:\n' + e);
+        return;
+      }
+      var count = Blockly.mainWorkspace.getAllBlocks().length;
+      if (count && confirm('是否清除编辑以载入新文件载入?')) {
+        Blockly.mainWorkspace.clear();
+      }
+      Blockly.Xml.domToWorkspace(Blockly.mainWorkspace, xml);
+    }
+    // Reset value of input after loading because Chrome will not fire
+    // a 'change' event if the same file is loaded again.
+    document.getElementById('load').value = '';
+  };
+  reader.readAsText(files[0]);
+}
+
+/**
+ * Discard all blocks from the workspace.
+ */
+function discard() {
+  var count = Blockly.mainWorkspace.getAllBlocks().length;
+  if (count < 2 || window.confirm('清除这 ' + count + ' 块吗?')) {
+    Blockly.mainWorkspace.clear();
+    renderContent();
+  }
+}
+
+/*
+ * auto save and restore blocks
+ */
+function auto_save_and_restore_blocks() {
+  // Restore saved blocks in a separate thread so that subsequent
+  // initialization is not affected from a failed load.
+  window.setTimeout(restore_blocks, 0);
+  // Hook a save function onto unload.
+  bindEvent(window, 'unload', backup_blocks);
+  tabClick(selected);
+
+  // Init load event.
+  var loadInput = document.getElementById('load');
+  loadInput.addEventListener('change', load, false);
+  document.getElementById('fakeload').onclick = function() {
+    loadInput.click();
+  };
+}
+
+/**
+ * Bind an event to a function call.
+ * @param {!Element} element Element upon which to listen.
+ * @param {string} name Event name to listen to (e.g. 'mousedown').
+ * @param {!Function} func Function to call when event is triggered.
+ *     W3 browsers will call the function with the event object as a parameter,
+ *     MSIE will not.
+ */
+function bindEvent(element, name, func) {
+  if (element.addEventListener) {  // W3C
+    element.addEventListener(name, func, false);
+  } else if (element.attachEvent) {  // IE
+    element.attachEvent('on' + name, func);
+  }
+}
+
+//loading examples via ajax
+var ajax;
+function createAJAX() {
+  if (window.ActiveXObject) { //IE
+    try {
+      return new ActiveXObject("Msxml2.XMLHTTP");
+    } catch (e) {
+      try {
+        return new ActiveXObject("Microsoft.XMLHTTP");
+      } catch (e2) {
+        return null;
+      }
+    }
+  } else if (window.XMLHttpRequest) {
+    return new XMLHttpRequest();
+  } else {
+    return null;
+  }
+}
