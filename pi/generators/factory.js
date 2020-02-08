264fc6de03f633065fd1f3febb457b754c3719e2
王逸伦 <594352301@qq.com>
2020-02-08 20:06:18 +0800

    SVN Update

--- pi/generators/factory.js
+++
@@ -1,8 +0,0 @@
-Blockly.Python.factory_do=function(){
-	return this.getFieldValue('VALUE')+"\n";
-};
-
-Blockly.Python.factory_import = function() {
-	var INCLUDE = this.getFieldValue('INCLUDE');
-	return 'import '+INCLUDE+'\n';
-};
