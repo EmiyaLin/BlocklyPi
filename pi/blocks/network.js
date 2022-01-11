aa3af07a2d059dcb6527d9ffd86c776c6aed77fb
hgcserver <594352301@qq.com>
2022-01-11 07:16:26 +0000

    增加网络模块

---
+++ pi/blocks/network.js
@@ -0,0 +1,80 @@
+Blockly.Blocks.network_get={
+    init:function(){
+        this.setColour(0);
+        this.appendValueInput("URL",String)
+			.appendField("执行GET请求 URL")
+            .setCheck("String");
+        this.setPreviousStatement(true);
+        this.setNextStatement(true);
+    }
+};
+
+Blockly.Blocks.network_getout={
+    init:function(){
+        this.setColour(0);
+        this.appendValueInput("URL",String)
+			.appendField("执行GET请求并返回数据 URL")
+            .setCheck("String");
+        this.setOutput(true,"String");
+    }
+};
+
+Blockly.Blocks.network_tcp_init={
+    init:function(){
+        this.setColour(0);
+        this.appendDummyInput("")
+            .appendField("TCP客户端")
+            .appendField(new Blockly.FieldTextInput('tcp'),'TCP_OBJ')
+            .appendField("初始化");
+        this.appendValueInput("HOST",String)
+			.appendField("主机地址")
+            .setCheck("String");
+        this.appendValueInput("PORT",Number)
+			.appendField("端口号")
+            .setCheck("Number");
+        this.setPreviousStatement(true);
+        this.setNextStatement(true);
+    }
+};
+
+Blockly.Blocks.network_tcp_close={
+    init:function(){
+        this.setColour(0);
+        this.appendDummyInput("")
+            .appendField("TCP客户端")
+            .appendField(new Blockly.FieldTextInput('tcp'),'TCP_OBJ')
+            .appendField("关闭客户端");
+        this.setPreviousStatement(true);
+        this.setNextStatement(true);
+    }
+};
+
+Blockly.Blocks.network_tcp_recv={
+    init:function(){
+	    this.setColour(0);
+	    this.appendDummyInput("")
+            .appendField("TCP客户端")
+            .appendField(new Blockly.FieldTextInput('tcp'),'TCP_OBJ')
+            .appendField("接收数据");
+        this.appendValueInput("SIZE",Number)
+            .setCheck("Number");
+        this.appendDummyInput()
+            .appendField("字节");
+        this.setOutput(true,"String");
+        this.setInputsInline(true);
+    }
+};
+
+Blockly.Blocks.network_tcp_send={
+    init:function(){
+	    this.setColour(0);
+	    this.appendDummyInput("")
+            .appendField("TCP客户端")
+            .appendField(new Blockly.FieldTextInput('tcp'),'TCP_OBJ');
+        this.appendValueInput("DATA")
+            .appendField("发送数据")
+            .setCheck("String");
+        this.setPreviousStatement(true);
+	    this.setNextStatement(true);
+    }
+};
---
+++ pi/generators/network.js
@@ -0,0 +1,39 @@
+Blockly.Python.network_get=function(){
+    Blockly.Python.definitions_['import urllib2'] = 'import urllib2';
+    var url=Blockly.Python.valueToCode(this,'URL',Blockly.Python.ORDER_ATOMIC);
+	return "urllib2.urlopen(urllib2.Request("+url+"))\n";
+};
+
+Blockly.Python.network_getout=function(){
+    Blockly.Python.definitions_['import urllib2'] = 'import urllib2';
+    var url=Blockly.Python.valueToCode(this,'URL',Blockly.Python.ORDER_ATOMIC);
+	return ["urllib2.urlopen(urllib2.Request("+url+"))",Blockly.Python.ORDER_ATOMIC];
+};
+
+Blockly.Python.network_tcp_init=function(){
+	Blockly.Python.definitions_['import socket'] = 'import socket';
+	var TCP_OBJ=this.getFieldValue('TCP_OBJ');
+	var HOST=Blockly.Python.valueToCode(this,'HOST',Blockly.Python.ORDER_ATOMIC);
+	var PORT=Blockly.Python.valueToCode(this,'PORT',Blockly.Python.ORDER_ATOMIC);
+	return TCP_OBJ+"=socket.socket(socket.AF_INET,socket.SOCK_STREAM)\n"+TCP_OBJ+".connect(("+HOST+","+PORT+"))\n";
+};
+
+Blockly.Python.network_tcp_close=function(){
+	Blockly.Python.definitions_['import socket'] = 'import socket';
+    var TCP_OBJ=this.getFieldValue('TCP_OBJ');
+	return TCP_OBJ+".close()\n";
+};
+
+Blockly.Python.network_tcp_recv=function(){
+	Blockly.Python.definitions_['import socket'] = 'import socket';
+    var TCP_OBJ=this.getFieldValue('TCP_OBJ');
+    var SIZE=Blockly.Python.valueToCode(this,'SIZE',Blockly.Python.ORDER_ATOMIC);
+	return [TCP_OBJ+".recv("+SIZE+")",Blockly.Python.ORDER_ATOMIC];
+};
+
+Blockly.Python.network_tcp_send=function(){
+	Blockly.Python.definitions_['import socket'] = 'import socket';
+    var TCP_OBJ=this.getFieldValue('TCP_OBJ');
+	var DATA=Blockly.Python.valueToCode(this,'DATA',Blockly.Python.ORDER_ATOMIC);
+	return TCP_OBJ+".sendall("+DATA+")\n";
+};
