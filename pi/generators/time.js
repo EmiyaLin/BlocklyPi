64a3955d53e7fc026e69451cf344251c3e217102
hgcserver <594352301@qq.com>
2022-01-11 03:15:17 +0000

    小修补

--- pi/generators/time.js
+++ pi/generators/time.js
@@ -9,3 +9,14 @@ Blockly.Python.time_delay_ms=function(){
     var delay_time=Blockly.Python.valueToCode(this,'DELAY_TIME',Blockly.Python.ORDER_ATOMIC)||'1';
     return 'time.sleep('+delay_time+'/1000)\n';
 };
+
+Blockly.Python.time_now=function(){
+    Blockly.Python.definitions_['import datetime'] = 'import datetime';
+    var mode=this.getFieldValue("MODE");
+    return ['datetime.datetime.now().'+mode,Blockly.Python.ORDER_ATOMIC];
+};
+
+Blockly.Python.time_time=function(){
+    Blockly.Python.definitions_['import time'] = 'import time';
+    return ['time.time()',Blockly.Python.ORDER_ATOMIC];
+};
