b0ba3c037f916dc5362dcc97d0f9f82458af3a2c
hgcserver <594352301@qq.com>
2022-01-11 02:52:17 +0000

    小变动

--- pi/generators/console.js
+++ pi/generators/console.js
@@ -17,5 +17,5 @@ Blockly.Python.console_shell=function(){
 Blockly.Python.console_shellout=function(){
     Blockly.Python.definitions_['import commands'] = "import commands";
     var shell=Blockly.Python.valueToCode(this,'SHELL',Blockly.Python.ORDER_ATOMIC)||'1';
-    return ['commands.getoutput('+shell+')\n',Blockly.Python.ORDER_ATOMIC];
+    return ['commands.getoutput('+shell+')',Blockly.Python.ORDER_ATOMIC];
 };
