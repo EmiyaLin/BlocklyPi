ca2cbb2150e3077378bfe4c60386f779cacccd53
hgcserver <594352301@qq.com>
2022-01-12 03:38:36 +0000

    小变动

--- pi/blocks/factory.js
+++ pi/blocks/factory.js
@@ -1,19 +1,19 @@
-Blockly.Blocks.factory_do={
-    init:function(){
-        this.setColour(75);
-        this.appendDummyInput("")
-            .appendField(new Blockly.FieldTextInput('print(\"Hello world!\")'),'VALUE');
-        this.setPreviousStatement(true);
-        this.setNextStatement(true);
-    }
-};
-
 Blockly.Blocks.factory_import={
     init:function(){
         this.setColour(75);
         this.appendDummyInput("")
             .appendField("import ")
             .appendField(new Blockly.FieldTextInput('time'), 'INCLUDE');
+        this.setPreviousStatement(true);
+        this.setNextStatement(true);
+    }
+};
+
+Blockly.Blocks.factory_do={
+    init:function(){
+        this.setColour(75);
+        this.appendDummyInput("")
+            .appendField(new Blockly.FieldTextInput('print(\"Hello world!\")'),'VALUE');
         this.setPreviousStatement(true);
         this.setNextStatement(true);
     }
--- pi/generators/factory.js
+++ pi/generators/factory.js
@@ -1,11 +1,10 @@
-Blockly.Python.factory_do=function(){
-	return this.getFieldValue('VALUE')+"\n";
-};
-
 Blockly.Python.factory_import=function(){
 	var INCLUDE=this.getFieldValue('INCLUDE');
-	Blockly.Python.definitions_['import '+INCLUDE]='import '+INCLUDE;
-	return "";
+	return 'import '+INCLUDE;
+};
+
+Blockly.Python.factory_do=function(){
+	return this.getFieldValue('VALUE')+"\n";
 };
 
 Blockly.Python.factory_return=function(){
