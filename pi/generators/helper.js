271cc8b5ba93eab0db70c4c30eee1c5966fc61ef
hgcserver <594352301@qq.com>
2022-01-12 04:04:44 +0000

    小修补

--- pi/generators/helper.js
+++ pi/generators/helper.js
@@ -3,21 +3,21 @@ Blockly.Python.helper_break=function(){
 };
 
 Blockly.Python.helper_int=function(){
-    var INPUT=this.getFieldValue('INPUT');
+    var INPUT=Blockly.Python.valueToCode(this,'INPUT',Blockly.Python.ORDER_ATOMIC);
     return ["int("+INPUT+")",Blockly.Python.ORDER_ATOMIC];
 };
 
 Blockly.Python.helper_long=function(){
-    var INPUT=this.getFieldValue('INPUT');
+    var INPUT=Blockly.Python.valueToCode(this,'INPUT',Blockly.Python.ORDER_ATOMIC);
     return ["long("+INPUT+")",Blockly.Python.ORDER_ATOMIC];
 };
 
 Blockly.Python.helper_float=function(){
-    var INPUT=this.getFieldValue('INPUT');
+    var INPUT=Blockly.Python.valueToCode(this,'INPUT',Blockly.Python.ORDER_ATOMIC);
     return ["float("+INPUT+")",Blockly.Python.ORDER_ATOMIC];
 };
 
 Blockly.Python.helper_str=function(){
-    var INPUT=this.getFieldValue('INPUT');
+    var INPUT=Blockly.Python.valueToCode(this,'INPUT',Blockly.Python.ORDER_ATOMIC);
     return ["str("+INPUT+")",Blockly.Python.ORDER_ATOMIC];
 };
