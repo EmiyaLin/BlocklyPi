60aec0342e2ccc8fcb5cf9255b87167b76ec9ca9
hgcserver <594352301@qq.com>
2022-01-12 03:27:06 +0000

    小变动

--- pi/blocks/factory.js
+++ pi/blocks/factory.js
@@ -1,6 +1,6 @@
 Blockly.Blocks.factory_do={
     init:function(){
-        this.setColour(85);
+        this.setColour(75);
         this.appendDummyInput("")
             .appendField(new Blockly.FieldTextInput('print(\"Hello world!\")'),'VALUE');
         this.setPreviousStatement(true);
@@ -10,7 +10,7 @@ Blockly.Blocks.factory_do={
 
 Blockly.Blocks.factory_import={
     init:function(){
-        this.setColour(85);
+        this.setColour(75);
         this.appendDummyInput("")
             .appendField("import ")
             .appendField(new Blockly.FieldTextInput('time'), 'INCLUDE');
@@ -21,7 +21,7 @@ Blockly.Blocks.factory_import={
 
 Blockly.Blocks.factory_return={
     init:function(){
-        this.setColour(85);
+        this.setColour(75);
         this.appendDummyInput("")
             .appendField(new Blockly.FieldTextInput('test'), 'VALUE');
         this.setOutput(true);
