af4e6fc739aa670a03483fd0d2efae6d6775afd7
hgcserver <594352301@qq.com>
2022-01-14 08:20:45 +0000

    增加步进电机模块

---
+++ pi/blocks/device.js
@@ -0,0 +1,42 @@
+Blockly.Blocks.device_stepper_init={
+    init:function(){
+        this.setColour(100);
+        this.appendDummyInput("")
+            .appendField("步进电机")
+            .appendField(new Blockly.FieldTextInput('stepper'),'STEPPER_OBJ')
+            .appendField("初始化");
+        for(let i=1;i<=4;i++){
+            this.appendValueInput("PIN"+i)
+                .appendField("管脚#"+i,Number)
+                .setCheck("Number")
+                .setAlign(Blockly.ALIGN_RIGHT);
+        };
+        this.appendValueInput("STEPS")
+            .appendField("每转步数",Number)
+            .setCheck("Number")
+            .setAlign(Blockly.ALIGN_RIGHT);
+        this.appendValueInput("SPEED")
+            .appendField("设置转速",Number)
+            .setCheck("Number")
+            .setAlign(Blockly.ALIGN_RIGHT);
+        this.setPreviousStatement(true);
+        this.setNextStatement(true);
+        this.setInputsInline(false);
+    }
+};
+
+Blockly.Blocks.device_stepper_move={
+    init:function(){
+        this.setColour(100);
+        this.appendDummyInput("")
+            .appendField("步进电机")
+            .appendField(new Blockly.FieldTextInput('stepper'),'STEPPER_OBJ');
+        this.appendValueInput("STEPS")
+            .appendField("移动步数",Number)
+            .setCheck("Number")
+            .setAlign(Blockly.ALIGN_RIGHT);
+        this.setPreviousStatement(true);
+        this.setNextStatement(true);
+        this.setInputsInline(true);
+    }
+};
---
+++ pi/generators/device.js
@@ -0,0 +1,20 @@
+Blockly.Python.device_stepper_init=function(){
+    Blockly.Python.definitions_['import RPi.GPIO as GPIO'] = "import RPi.GPIO as GPIO\nGPIO.setmode(GPIO.BOARD)\nGPIO.setwarnings(False)";
+    Blockly.Python.definitions_['from stepper import Stepper'] = 'from stepper import Stepper';
+    var STEPPER_OBJ=this.getFieldValue('STEPPER_OBJ');
+    var pin1=Blockly.Python.valueToCode(this,'PIN1',Blockly.Python.ORDER_ATOMIC)||1;
+    var pin2=Blockly.Python.valueToCode(this,'PIN2',Blockly.Python.ORDER_ATOMIC)||1;
+    var pin3=Blockly.Python.valueToCode(this,'PIN3',Blockly.Python.ORDER_ATOMIC)||1;
+    var pin4=Blockly.Python.valueToCode(this,'PIN4',Blockly.Python.ORDER_ATOMIC)||1;
+    var steps=Blockly.Python.valueToCode(this,'STEPS',Blockly.Python.ORDER_ATOMIC)||1;
+    var speed=Blockly.Python.valueToCode(this,'SPEED',Blockly.Python.ORDER_ATOMIC)||1;
+    return STEPPER_OBJ+"=Stepper("+steps+","+pin1+","+pin2+","+pin3+","+pin4+")\n"+STEPPER_OBJ+".setSpeed("+speed+")\n";
+};
+
+Blockly.Python.device_stepper_move=function(){
+    Blockly.Python.definitions_['import RPi.GPIO as GPIO'] = "import RPi.GPIO as GPIO\nGPIO.setmode(GPIO.BOARD)\nGPIO.setwarnings(False)";
+    Blockly.Python.definitions_['from stepper import Stepper'] = 'from stepper import Stepper';
+    var STEPPER_OBJ=this.getFieldValue('STEPPER_OBJ');
+    var steps=Blockly.Python.valueToCode(this,'STEPS',Blockly.Python.ORDER_ATOMIC)||1;
+    return STEPPER_OBJ+".step("+steps+")\n";
+};
