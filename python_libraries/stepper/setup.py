328020d7a0fdcd5b77bff778a150c6613dce0f41
hgcserver <594352301@qq.com>
2022-01-15 10:39:38 +0000

    Update

---
+++ python_libraries/stepper/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup(
+    name='stepper',
+    version='1.0',
+    description='stepper library',
+    author='hgcserver',
+    author_email='594352301@qq.com',
+    url='https://gitee.com/hgcserver',
+    py_modules=['stepper']
+)
