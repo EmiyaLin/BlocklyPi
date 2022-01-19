bc5bbac4c18ec78545dd58b6095b0ed7bed569a5
hgcserver <594352301@qq.com>
2022-01-19 04:33:16 +0000

    Update

--- python_libraries/bmp180/bmp180_test.py
+++ python_libraries/bmp180/bmp180_test.py
@@ -1,7 +1,5 @@
 from bmp180 import BMP180
 bmp180=BMP180(1,0x77)
-while True:
-    print("Temperature:"+str(bmp180.read_temperature()))
-    print("Pressure:"+str(bmp180.read_pressure())+" Pa")
-    print("")
-    time.sleep(1)
+print("Temperature:"+str(bmp180.read_temperature()))
+print("Pressure:"+str(bmp180.read_pressure())+" Pa")
+print("")
