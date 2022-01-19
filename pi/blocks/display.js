3c099f42350b7ff455917112a8362ec8ac86c065
hgcserver <594352301@qq.com>
2022-01-19 07:37:47 +0000

    增加OLED模块

--- pi/blocks/display.js
+++ pi/blocks/display.js
@@ -66,3 +66,78 @@ Blockly.Blocks.display_lcd1602_clear={
         this.setNextStatement(true);
     }
 };
+
+Blockly.Blocks.display_oled_init={
+    init:function(){
+        this.setColour(180);
+        this.appendDummyInput("")
+            .appendField("显示器OLED")
+            .appendField(new Blockly.FieldTextInput('myoled'), 'OLED_OBJ')
+            .appendField("初始化");
+        this.appendValueInput("BUS",Number)
+			.appendField("总线")
+            .setCheck("Number");
+        this.appendValueInput("OLED_ADDR",Number)
+            .appendField("设备地址")
+            .setCheck("Number");
+        this.setPreviousStatement(true);
+        this.setNextStatement(true);
+        this.setInputsInline(true);
+    }
+};
+
+Blockly.Blocks.display_oled_print={
+    init:function(){
+        this.setColour(180);
+        this.appendDummyInput("")
+            .appendField("显示器OLED")
+            .appendField(new Blockly.FieldTextInput('myoled'), 'OLED_OBJ');
+        this.appendValueInput("x",Number)
+			.appendField("在坐标 x:")
+            .setCheck("Number");
+        this.appendValueInput("y",Number)
+			.appendField("y:")
+            .setCheck("Number");
+        this.appendValueInput("STR",String)
+			.appendField("处打印")
+			.setCheck("String");
+        this.setPreviousStatement(true);
+        this.setNextStatement(true);
+        this.setInputsInline(true);
+    }
+};
+
+Blockly.Blocks.display_oled_printline={
+    init:function(){
+        this.setColour(180);
+        this.appendDummyInput("")
+            .appendField("显示器OLED")
+            .appendField(new Blockly.FieldTextInput('myoled'), 'OLED_OBJ');
+        this.appendValueInput("STR1",String)
+			.appendField("打印在第1行")
+            .setCheck("String");
+        this.appendValueInput("STR2",String)
+			.appendField("打印在第2行")
+            .setCheck("String");
+        this.appendValueInput("STR3",String)
+			.appendField("打印在第3行")
+            .setCheck("String");
+        this.appendValueInput("STR4",String)
+			.appendField("打印在第4行")
+			.setCheck("String");
+        this.setPreviousStatement(true);
+        this.setNextStatement(true);
+    }
+};
+
+Blockly.Blocks.display_oled_clear={
+    init:function(){
+        this.setColour(180);
+        this.appendDummyInput("")
+            .appendField("显示器OLED")
+            .appendField(new Blockly.FieldTextInput('myoled'), 'OLED_OBJ')
+            .appendField("清屏");
+        this.setPreviousStatement(true);
+        this.setNextStatement(true);
+    }
+};
--- pi/generators/display.js
+++ pi/generators/display.js
@@ -28,3 +28,36 @@ Blockly.Python.display_lcd1602_clear=function(){
     var lcd_obj=this.getFieldValue('LCD_OBJ');
 	return lcd_obj+".clear_lcd()\n";
 };
+
+Blockly.Python.display_oled_init=function(){
+    Blockly.Python.definitions_['from ssd1306 import SSD1306'] = "from ssd1306 import SSD1306";
+    var oled_obj=this.getFieldValue('OLED_OBJ');
+    var bus=Blockly.Python.valueToCode(this,'BUS',Blockly.Python.ORDER_ATOMIC);
+    var oled_addr=Blockly.Python.valueToCode(this,'OLED_ADDR',Blockly.Python.ORDER_ATOMIC);
+	return oled_obj+"=SSD1306("+bus+","+oled_addr+")\n";
+};
+
+Blockly.Python.display_oled_print=function(){
+    Blockly.Python.definitions_['from ssd1306 import SSD1306'] = "from ssd1306 import SSD1306";
+    var oled_obj=this.getFieldValue('OLED_OBJ');
+    var x=Blockly.Python.valueToCode(this,'x',Blockly.Python.ORDER_ATOMIC);
+    var y=Blockly.Python.valueToCode(this,'y',Blockly.Python.ORDER_ATOMIC);
+    var str=Blockly.Python.valueToCode(this,'STR',Blockly.Python.ORDER_ATOMIC);
+	return oled_obj+".ShowString("+x+","+y+","+str+")\n";
+};
+
+Blockly.Python.display_oled_printline=function(){
+    Blockly.Python.definitions_['from ssd1306 import SSD1306'] = "from ssd1306 import SSD1306";
+    var oled_obj=this.getFieldValue('OLED_OBJ');
+    var str1=Blockly.Python.valueToCode(this,'STR1',Blockly.Python.ORDER_ATOMIC);
+    var str2=Blockly.Python.valueToCode(this,'STR2',Blockly.Python.ORDER_ATOMIC);
+    var str3=Blockly.Python.valueToCode(this,'STR3',Blockly.Python.ORDER_ATOMIC);
+    var str4=Blockly.Python.valueToCode(this,'STR4',Blockly.Python.ORDER_ATOMIC);
+	return oled_obj+".ShowString(0,0,"+str1+")\n"+oled_obj+".ShowString(0,2,"+str2+")\n"+oled_obj+".ShowString(0,4,"+str3+")\n"+oled_obj+".ShowString(0,6,"+str4+")\n";
+};
+
+Blockly.Python.display_oled_clear=function(){
+    Blockly.Python.definitions_['from ssd1306 import SSD1306'] = "from ssd1306 import SSD1306";
+    var oled_obj=this.getFieldValue('OLED_OBJ');
+	return oled_obj+".Clear()\n";
+};
