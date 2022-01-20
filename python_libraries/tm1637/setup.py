43cd4279120ca6bec8ab4ced4eee834a0a8ca25a
hgcserver <594352301@qq.com>
2022-01-20 06:14:23 +0000

    增加tm1637的库

---
+++ python_libraries/tm1637/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup(
+    name='tm1637',
+    version='1.0',
+    description='tm1637 library',
+    author='hgcserver',
+    author_email='594352301@qq.com',
+    url='https://gitee.com/hgcserver',
+    py_modules=['tm1637']
+)
